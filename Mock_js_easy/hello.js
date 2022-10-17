47eacbaebf10e364f24c0964fcd67ad2b51a53bb
ShivamNautiyal1994 <112508548+ShivamNautiyal1994@users.noreply.github.com>
2022-10-17 16:10:23 +0530

    Delete Mock_js_easy directory

--- Mock_js_easy/hello.js
+++
@@ -1,7 +0,0 @@
-// let name= document.getElementById('#name')
-// let email= document.getElementById('#email')
-function f1(){
-    let name= document.getElementById('name')
-let email= document.getElementById('email')
-alert(`name is ${name.value} and email is ${email.value}`)
-}
