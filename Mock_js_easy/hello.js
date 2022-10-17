ff3e8289ccb646e73e37936a44c702f2bed74b17
ShivamNautiyal1994 <shivamnautiyalce2016@gmail.com>
2022-10-17 16:03:25 +0530

    initial

---
+++ Mock_js_easy/hello.js
@@ -0,0 +1,7 @@
+// let name= document.getElementById('#name')
+// let email= document.getElementById('#email')
+function f1(){
+    let name= document.getElementById('name')
+let email= document.getElementById('email')
+alert(`name is ${name.value} and email is ${email.value}`)
+}
